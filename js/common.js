4ac48e70f521d8337fd71a4173cfcecbeffb9c78
김성은 <kse012050@naver.com>
2021-01-29 17:27:01 +0900

    메인 페이지 베너 슬라이더를 만들었다

--- js/common.js
+++ js/common.js
@@ -1,20 +1,15 @@
-(function(){
+// (function(){
 
-var marker = document.querySelector('.subMenuBar');
-var item = document.querySelectorAll('.subMenuArea');
+var subMenuBar = document.querySelector('.subMenuBar');
+var subMenu = document.querySelectorAll('.subMenuArea ul li');
 
 function indicator(e){
-    marker.style.left = e.offsetLeft + "px";
-    marker.style.width = e.offsetWidth + "px";
+    subMenuBar.style.left = e.offsetLeft + "px";
+    subMenuBar.style.width = e.offsetWidth + "px";
 }
-item.forEach(link => {
+subMenu.forEach(link => {
     link.addEventListener('click',(e) => {
-        alert();
         indicator(e.target);
     });
 })
-console.log(item);
-// item.addEventListener('click',function(){
-// })
-// alert();
-})();
+// })();
