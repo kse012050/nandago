fa7d75071af6bb8e6477fd62dd7a657fa1e6c61d
김성은 <kse012050@naver.com>
2021-02-01 08:50:11 +0900

    subMenuBar 시작 지점을 완성했다

--- js/common.js
+++ js/common.js
@@ -1,15 +1,16 @@
-// (function(){
-
 var subMenuBar = document.querySelector('.subMenuBar');
 var subMenu = document.querySelectorAll('.subMenuArea ul li');
+var startSubMenuBar = document.querySelectorAll('.subMenuArea ul li.active');
 
 function indicator(e){
     subMenuBar.style.left = e.offsetLeft + "px";
     subMenuBar.style.width = e.offsetWidth + "px";
+    console.log(e);
 }
+
+indicator(startSubMenuBar[0]);
 subMenu.forEach(link => {
     link.addEventListener('click',(e) => {
         indicator(e.target);
     });
 })
-// })();
